bfbeb82d4bab0ba8a949bf54e841d31f8df81ea2
Rayane Nordine <rayanenordine@gmail.com>
2022-05-17 21:30:27 +0200

    add redis

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,8 @@ class Task < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  # after_create_commit -> { broadcast_prepend_later_to "tasks" }
+  # after_update_commit -> { broadcast_replace_later_to "tasks" }
+  # after_destroy_commit -> { broadcast_remove_to "tasks" }
 end
